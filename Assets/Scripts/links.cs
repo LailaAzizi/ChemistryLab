99991ed71538206c7c6ff7e5e51c0642c9a67ed7
LailaAzizi <42999149+LailaAzizi@users.noreply.github.com>
2018-09-22 14:49:55 +0430

    first commit

---
+++ Assets/Scripts/links.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class links : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Linkedin(){
+
+		Application.OpenURL ("https://www.linkedin.com/in/leila-azizi-a80698156/");
+	}
+
+	public void Twitter(){
+
+		Application.OpenURL ("https://twitter.com/Leila45_Azizi");
+	}
+
+
+	public void Github(){
+
+		Application.OpenURL ("https://github.com/KimiaAzizi");
+	}
+
+   
+}
